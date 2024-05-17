4b3d4c7fd3482567f73918dc0c44cc7ea6ebaa30
YOLO-GIT <ahmadsufi345@gmail.com>
2024-05-17 23:44:06 +0800

    Some Script Changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,7 @@ module.exports = {
                         transform: 'translateX(0)'
                     },
                     '100%': {
-                        transform: 'translateX(-70%)'
+                        transform: 'translateX(-81%)'
                     },
                 },
             },
